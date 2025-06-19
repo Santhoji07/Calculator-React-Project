b7f5b5ab3aabeaafe70e1dd8ae50e20526949357
Santhoji07 <santhoji07msd@gmail.com>
2025-06-19 09:45:47 +0530

    count commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Greet from './greet'; 
+import React, { useState } from 'react';
+//javascript XML 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  /*return   <>
+  <h1> Welcome</h1>
+ 
+  <Greet name="Santhoji"/><div>
+      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fbeautiful%2F&psig=AOvVaw1Cf6OIfANE86UXJHPcwcT3&ust=1750329282819000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLi11uvi-o0DFQAAAAAdAAAAABAE" />
     </div>
-  );
+</>*/
+  const [count, setCount] = useState(0);
+    return(
+        <div>
+            <p>you have clicked {count} times </p>
+        <button onClick={() => setCount(count + 1)}>Click me</button></div>
+    )
 }
 
 export default App;
---
+++ src/greet.js
@@ -0,0 +1,7 @@
+function Greet({name}){
+    
+    //return (<p> Hello {name}<img src={src} alt="Description"  alt="Beautiful"/></p>);
+
+}
+
+export default Greet;
