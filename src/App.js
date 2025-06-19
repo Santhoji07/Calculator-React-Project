8260433640431eea41144f9afecc07c0fbc24ba0
Santhoji07 <santhoji07msd@gmail.com>
2025-06-19 10:10:21 +0530

    list commit

--- src/App.js
+++ src/App.js
@@ -2,20 +2,47 @@ import './App.css';
 import Greet from './greet'; 
 import React, { useState } from 'react';
 //javascript XML 
-function App() {
+//function App() {
   /*return   <>
   <h1> Welcome</h1>
  
   <Greet name="Santhoji"/><div>
       <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fbeautiful%2F&psig=AOvVaw1Cf6OIfANE86UXJHPcwcT3&ust=1750329282819000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLi11uvi-o0DFQAAAAAdAAAAABAE" />
     </div>
-</>*/
+</>
   const [count, setCount] = useState(0);
     return(
         <div>
             <p>you have clicked {count} times </p>
         <button onClick={() => setCount(count + 1)}>Click me</button></div>
-    )
+    )*/
+
+       /* function handleClick() {
+            alert('Button clicked!');
+        }
+        return<button onClick={handleClick}>Click Me</button>
+}*/
+
+/*function App(isLoggedIn) {
+  return isLoggedIn?<h4>Successfully loged in</h4>:<h4>Login failed</h4>;
+  
+}*/
+
+function App() {
+  const names=['a','b','c','d','e'];
+  return(
+    <ul>
+      {
+        names.map((name,index)=>(
+          <li key={index}>{name}</li>
+        )
+      )
+      }
+    </ul>
+  )
 }
+
+
+
 
 export default App;
